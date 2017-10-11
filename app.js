c5bbf89788e1a456b8f095eb14a88211fb5b27ab
bool15 <andrewnemeth04@gmail.com>
2017-10-11 17:39:11 -0400

    Added live GMview

--- app.js
+++ app.js
@@ -37,11 +37,12 @@ var DBmodels = require('./DBModels/DBmodels.js');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -63,6 +64,7 @@ app.use('/gmscreen', gmscreen);
 app.use('/charinput', charinput);
 app.use('/fullcharedit', fullcharedit);
 app.use('/logout', logout);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
